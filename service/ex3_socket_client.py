2488d628c89d52789402a97064e63d5e10f4f163
hungdcwindows <hungttmk53@gmail.com>
2018-01-25 17:16:02 +0700

    OK

--- service/ex3_socket_client.py
+++ service/ex3_socket_client.py
@@ -1,14 +1,15 @@
 import socket
 
 def main():
-	host_ip		= 'localhost'
+	host_ip		= '127.0.0.1'
 	host_port	= 12346
 	server		= ('1.52.48.85',12345)
 
 	s 			= socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 	s.bind((host_ip, host_port))
 
-	message		= input(">_")
+#	message		= input(">_")
+	message		= str('message')
 	
 	while message !='q':
 		s.sendto(message,server)
--- service/ex3_socket_server.py
+++ service/ex3_socket_server.py
@@ -1,22 +1,9 @@
 import socket
 
-def main():
-	host_ip 	= '1.52.48.85'
-	host_port 	= 12345
+HOST		= 'localhost'
+PORT	= 12345
+BUFSIZE		= 1024
+ADDR		= (HOST, PORT)
 
-	s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-	s.bind((host_ip, host_port))
-
-	print('Server Start')
-
-	while True:
-		data,addr = s.recvfrom(1024)
-		print("message From: " + str(addr))
-		print("from connection user: " + str(data))
-		data = str(data).upper()
-		print("sending: " + str(data))
-		s.sendto(data,addr)
-	c.close()
-
-if __name__ == '__main__':
-	main()
+server_socket = socket.socket()
+server_socket.bind(ADDR)
