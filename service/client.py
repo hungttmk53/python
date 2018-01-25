cde0ecb1c2f8efb160ee0d08b0a735728f5daa4b
hungdcwindows <hungttmk53@gmail.com>
2018-01-25 17:27:10 +0700

    OK

--- service/client.py
+++ service/client.py
@@ -6,5 +6,6 @@ host = '1.52.48.85'
 port = 12345
 
 s.connect((host, port))
+s.sendall(b'Hello Server')
 print(s.recv(1024))
 s.close()
--- service/service.py
+++ service/service.py
@@ -7,8 +7,10 @@ port = 12345
 s.bind((host,port))
 
 s.listen(5)
+
 while True:
 	c, addr = s.accept()
 	print('Ket noi tu: ', addr)
+	print(c.recv(1024))
 	c.send(b'Cam on.')
 	c.close()
