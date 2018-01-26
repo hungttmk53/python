46a04e3bcf9671750c517792187764cebfdbf126
hungdcwindows <hungttmk53@gmail.com>
2018-01-26 10:45:15 +0700

    10h45

--- service/client.py
+++ service/client.py
@@ -6,6 +6,8 @@ host = '1.52.48.85'
 port = 12345
 
 s.connect((host, port))
-s.sendall(b'Hello Server')
+string_send = input("Enter name: ")
+string_send = string_send.encode('utf-8')
+s.send(string_send)
 print(s.recv(1024))
 s.close()
